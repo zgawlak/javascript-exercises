4faf3b23c1a4b8aceb9a2081ca8209ce8550e2f3
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-25 13:25:49 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number' || num1 < 0 || num2 < 0) return 'ERROR';
 
+    if (num1 > num2){
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    let sum = 0;
+    for (num1; num1 <= num2; num1++){
+        sum += num1;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
