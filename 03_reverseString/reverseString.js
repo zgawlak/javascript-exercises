2ccef822214c70be29ceb4a5a363b79325735f0b
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-25 12:36:22 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let stringArray = [];
+    for (let i = 0; i < string.length; i++) {
+        stringArray.push(string.charAt(i));
+    }
+    let reversedArray = stringArray.reverse();
+    console.log(reversedArray);
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
