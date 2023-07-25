6dbeecda40524b996bbef65ca5050909ff05f311
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-25 13:43:20 -0400

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  num = (fahrenheit - 32) * (5/9);
+  return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  num = (celsius * (9/5) + 32);
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
