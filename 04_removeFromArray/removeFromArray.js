8ae08a4a99347326ee80efaccd715b13f190b855
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-25 13:02:11 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    for (const item of array) {
+        if (!args.includes(item)){
+            newArray.push(item);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
