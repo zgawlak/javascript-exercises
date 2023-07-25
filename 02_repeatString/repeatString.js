0ed7b59f1ede799b5c0491c6aa55fe597acc7901
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-25 12:20:22 -0400

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times >= 0) {
+        let repeatedString = '';
+        for (let i = 0; i < times; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
