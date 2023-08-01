3ce837d42aca8789bae2e75ffacfaa92954e4439
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-01 17:17:19 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 2; i <= count; i++) {
+        let current = num1 + num2;
+        num1 = num2;
+        num2 = current;
+    }
+    return num2;
 };
 
 // Do not edit below this line
