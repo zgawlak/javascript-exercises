ae27c6171ba7e1c39b3403972afecfdcd74d45ae
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-01 17:38:00 -0400

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    })
 
+    function getAge(yearOfBirth, yearOfDeath) {
+        if (!yearOfDeath) {
+            yearOfDeath = new Date().getFullYear();
+        }
+        return yearOfDeath - yearOfBirth;
+    }
 };
 
 // Do not edit below this line
